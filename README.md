d4d4272aa960df7970a80fb610ea22aaa1200cc9
Casey Link <me@caseylink.com>
2019-08-23 18:28:29 +0200

    Add column type feature

--- README.md
+++ README.md
@@ -29,21 +29,37 @@ $ scrapy crawl <spider name> -o sqlite.db -t sqlite
 
 ## Extra features
 
+
+### primary key
 By default the primary key is chosen more or less at random, since the order of
 keys in the dictionary is not guaranteed.
 
 To hint to the exporter which field should be a primary key define your Item
-class with a field containing `primary_key` key with a value of `True.
+class with a field containing `primary_key` key with a value of `True`.
 
 
-**Example:**
+See example.
+
+### Column Type
+
+By default all fields are created as `TEXT` columns. Hint to the exporter which
+column a field should be by specifying `sql_type`.
+
+See example.
+
+### Examples
 
 ```python
 class CheeseItem(scrapy.Item):
-
   id = scrapy.Field(primary_key=True)
   name = scrapy.Field()
   stinkyness = scrapy.Field(serializer=int)
+  crawled_at = scrapy.Field(sql_type="timestamp")
+```
 
 
+This item class produces the following schema:
+
+```sql
+CREATE TABLE [CheeseItem] ([name] text, [id] text, [crawled_at] timestamp, [stinkyness] text, primary key (id));
 ```
--- exporters.py
+++ exporters.py
@@ -6,6 +6,10 @@ from scrapy.exporters import BaseItemExporter
 import sqlite3
 
 
+def get_field(item, field_name):
+    return {} if isinstance(item, dict) else item.fields[field_name]
+
+
 class SqliteItemExporter(BaseItemExporter):
     def __init__(self, file, **kwargs):
         self._configure(kwargs)
@@ -18,28 +22,14 @@ class SqliteItemExporter(BaseItemExporter):
         item_class_name = type(item).__name__
 
         if item_class_name not in self.created_tables:
-            keys = None
-            if hasattr(item, "keys"):
-                sqlite_keys = list(item.keys())
-
-            pk = None
-            for field_name in item:
-                field = {} if isinstance(item, dict) else item.fields[field_name]
-                is_pk = field.get("primary_key", False)
-                if is_pk:
-                    pk = field_name
-                    break
-
-            self._create_table(
-                item_class_name, pk, iter(item.fields.keys()), sqlite_keys
-            )
+            self._create_table(item_class_name, item)
             self.created_tables.append(item_class_name)
 
         field_list = []
         value_list = []
         for field_name in item.keys():
             field_list.append("[%s]" % field_name)
-            field = item.fields[field_name]
+            field = get_field(item, field_name)
             value_list.append(self.serialize_field(field, field_name, item[field_name]))
 
         sql = "insert or ignore into [%s] (%s) values (%s)" % (
@@ -47,24 +37,33 @@ class SqliteItemExporter(BaseItemExporter):
             ", ".join(field_list),
             ", ".join(["?" for f in field_list]),
         )
+        print("SQL: ", sql)
+        print("VLIST", value_list)
         self.conn.execute(sql, value_list)
         self.conn.commit()
 
-    def _create_table(self, table_name, pk, columns, keys=None):
+    def _create_table(self, table_name, item):
         sql = "create table if not exists [%s] " % table_name
-
-        column_define = ["[%s] text" % column for column in columns]
-        print("type: %s" % type(keys))
+        pk = None
+        col_types = {}
+        for field_name in item:
+            field = get_field(item, field_name)
+            is_pk = field.get("primary_key", False)
+            if is_pk:
+                pk = field_name
+            sql_type = field.get("sql_type", "text")
+            col_types[field_name] = sql_type
+        column_define = [
+            "[{}] {}".format(column, sql_type) for column, sql_type in col_types.items()
+        ]
         if pk is not None:
             primary_key = "primary key ({})".format(pk)
             column_define.append(primary_key)
-        elif keys:
+        else:
+            keys = list(item.keys())
             if len(keys) > 0:
                 primary_key = "primary key ({})".format(keys[0])
                 column_define.append(primary_key)
-
-            # for key in keys[1:]:
-            # column_define.append('unique (%s)' % ', '.join(key))
 
         sql += "(%s)" % ", ".join(column_define)
 
